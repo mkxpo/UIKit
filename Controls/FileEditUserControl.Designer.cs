ac843dddc51e81277b498fcadc30bb3f92ef3467
user <user@mail.ru>
2022-05-21 17:08:54 +0300

    implemented User control for file blob field

---
+++ Controls/FileEditUserControl.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Core.Controls {
+    partial class FileEditUserControl {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txbFileName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLoad.Location = new System.Drawing.Point(708, 0);
+            this.btnLoad.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(150, 53);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Загрузить";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(870, 0);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(150, 53);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Все файлы|*.*";
+            this.openFileDialog1.RestoreDirectory = true;
+            this.openFileDialog1.ShowReadOnly = true;
+            // 
+            // txbFileName
+            // 
+            this.txbFileName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbFileName.AutoEllipsis = true;
+            this.txbFileName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbFileName.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.txbFileName.Location = new System.Drawing.Point(0, 0);
+            this.txbFileName.Name = "txbFileName";
+            this.txbFileName.Size = new System.Drawing.Size(699, 52);
+            this.txbFileName.TabIndex = 4;
+            this.txbFileName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.txbFileName.Click += new System.EventHandler(this.txbFileName_Click);
+            // 
+            // FileEditUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txbFileName);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnLoad);
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "FileEditUserControl";
+            this.Size = new System.Drawing.Size(1026, 55);
+            this.Resize += new System.EventHandler(this.FileEditUserControl_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label txbFileName;
+    }
+}
---
+++ Controls/FileEditUserControl.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Core.Controls {
+    public partial class FileEditUserControl : UserControl, ISupportInitialize {
+        public FileEditUserControl() {
+            BeginInit();
+            InitializeComponent();
+            EndInit();
+        }
+
+        byte[] blobData = null;
+
+        [DefaultValue(null)]
+        [Bindable(true)]
+        public byte[] BlobData {
+            get {
+                return blobData;
+            }
+            set {
+                if (blobData != value) {
+                    if (value != null && MaxBlobSize > 0 && value.Length > MaxBlobSize && !isInitializing) {
+                        throw new ArgumentException(string.Format("Размер загруженного файла превышает максимально допустимый размер {0} Мб.", MaxBlobSize / 1024.0 / 1024.0));
+                    }
+                    string fileName = FileNameFromBlob(value);
+                    blobData = value;                  
+                    txbFileName.Text = fileName;
+                    RefreshControlStatus();
+                }
+            }
+        }
+
+        [DefaultValue(null)]
+        public string FileName
+        {
+            get
+            {
+                return FileNameFromBlob(BlobData);
+            }
+        }
+
+        bool readOnly = false;
+
+        [DefaultValue(false)]
+        public bool ReadOnly {
+            get {
+                return readOnly;
+            }
+            set {
+                readOnly = value;
+                RefreshControlStatus();
+            }
+        }
+
+        int maxSize = 0;
+
+        [DefaultValue(0)]
+        public int MaxBlobSize {
+            get {
+                return maxSize;
+            }
+            set {
+                maxSize = value;
+            }
+        }
+
+        void RefreshControlStatus() {
+            btnLoad.Visible = !ReadOnly;
+            btnClear.Enabled = !ReadOnly && BlobData != null && BlobData.Length > 0;
+        }
+
+        string FileNameFromBlob(byte[] data) {
+            if (data == null || data.Length < 256) {
+                return null;
+            }
+            using (MemoryStream stream = new MemoryStream(data))
+            {
+                return System.Text.Encoding.UTF8.GetString(data, 0, 256);
+            }
+        }
+
+        byte[] CreateBlob(byte[] fileData, string fileName) {
+            using (var stream = new MemoryStream())
+            {
+                byte[] fileNameUtf8 = System.Text.Encoding.UTF8.GetBytes(fileName);
+                if (fileNameUtf8.Length <= 256)
+                {
+                    stream.Write(fileNameUtf8);
+                    byte[] zeroBytes = new byte[256 - fileNameUtf8.Length];
+                    stream.Write(zeroBytes);
+                }
+                else
+                {
+                    stream.Write(fileNameUtf8, 0, 256);
+                }
+                stream.Write(fileData);
+                return stream.ToArray();
+            }
+        }
+
+
+        void btnLoad_Click(object sender, EventArgs e) {
+            LoadFile();
+        }
+
+        bool isInitializing = false;
+        public void BeginInit() {
+            isInitializing = true;
+        }
+
+        public void EndInit() {
+            isInitializing = false;
+        }
+
+        void btnClear_Click(object sender, EventArgs e) {
+            BlobData = null;
+        }
+
+        private void FileEditUserControl_Resize(object sender, EventArgs e) {
+        }
+
+        void SaveFile()
+        {
+            using (var dlg = new SaveFileDialog())
+            {
+                dlg.FileName = FileName;
+                dlg.Filter = "Все файлы|*.*";
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    byte[] fileData = new byte[BlobData.Length - 256];
+                    Array.Copy(BlobData, 256, fileData, 0, BlobData.Length - 256);
+                    File.WriteAllBytes(dlg.FileName, fileData);
+                    var startInfo = new ProcessStartInfo()
+                    {
+                        FileName = dlg.FileName,
+                        WorkingDirectory = Path.GetDirectoryName(dlg.FileName),
+                        UseShellExecute = true
+                    };
+                    System.Diagnostics.Process.Start(startInfo);
+                }
+            }
+        }
+
+        void LoadFile()
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                UIHelper.Execute(this.ParentForm, () => {
+                    byte[] data = File.ReadAllBytes(openFileDialog1.FileName);
+                    BlobData = CreateBlob(data, Path.GetFileName(openFileDialog1.FileName));
+                });
+            }
+        }
+
+        private void txbFileName_Click(object sender, EventArgs e)
+        {
+            if (BlobData != null && BlobData.Length != 0)
+            {
+                SaveFile();
+            }
+            else
+            {
+                LoadFile();
+            }
+        }
+    }
+}
